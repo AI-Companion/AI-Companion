e961773a11244f09b999f78084de5cdc3196390d
mmarouen <azzouz.marouen@gmail.com>
2020-05-02 00:52:32 +0200

    2nd push

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 INSTALL_REQUIREMENTS = [
@@ -6,14 +6,18 @@ INSTALL_REQUIREMENTS = [
     'pandas',
     'pytest',
     'scikit-learn',
-    'matplotlib'
+    'matplotlib',
+    'flask',
+    'flask_restful'
 ]
 setup(name='marabou',
-      packages=['marabou'],
+      packages=find_packages(),
       version='0.0.1dev1',
       entry_points={
           'console_scripts': ['marabou-train=marabou.scripts.train_chain:main',
-                              'marabou-eval=marabou.scripts.eval_chain:main']
+                              'marabou-eval=marabou.scripts.eval_chain:main',
+                              'marabou-rest-api=marabou.scripts.serve_rest:main']
       },
-      install_requires=INSTALL_REQUIREMENTS
-      )
+      install_requires=INSTALL_REQUIREMENTS,
+      package_data={},
+      include_package_data=True)
