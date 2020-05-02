b547b691bedee837a54293f82661cef9e86cb56e
Youssef Azzouz <youssef.azzouz1512@gmail.com>
2020-05-02 19:09:48 +0100

    fixing cli deployment

--- marabou/scripts/serve_rest.py
+++ marabou/scripts/serve_rest.py
@@ -1,8 +1,9 @@
 import pickle
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
 from flask_restful import reqparse, abort, Api, Resource
 import argparse
 from marabou.dumb_model import DumbModel
+import sys
 
 app = Flask(__name__)
 api = Api(app)
@@ -30,10 +31,28 @@ class PredictSentiment(Resource):
         output = {'class0_probs': probs[0][0], 'class1_probs': probs[0][1]}
         return output
 
+
+@app.route('/',methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        model = DumbModel.deserialize('models/modelfile.pickle')
+        new_prediction = PredictSentiment(model=model) # new instance of PredictSentiment
+        output = new_prediction.model.predict_proba([task_content])
+        return render_template('index.html', output=output)
+    else:
+        return render_template('index.html')
+        
+
+
+
 def main():
-    args = parse_arguments()
-    model = DumbModel.deserialize(args.model_file)  # load model at the beginning once only
-    api.add_resource(PredictSentiment, '/',resource_class_args={model})
+    if len(sys.argv) < 2:
+        print("no args added")
+    else:
+        args = parse_arguments()
+        model = DumbModel.deserialize(args.model_file)  # load model at the beginning once only
+        api.add_resource(PredictSentiment, '/',resource_class_args={model})
     app.run(debug=True)
 
 if __name__ == '__main__':
