a6ef79d4444bea1b298f338dae27ae0d8ea0f2e5
Youssef Azzouz <youssef.azzouz1512@gmail.com>
2020-05-02 19:09:48 +0100

    fixing cli deployment

--- setup.py
+++ setup.py
@@ -14,12 +14,12 @@ setup(name='marabou',
     #package_dir={'':'marabou'},
     packages=find_packages(include=['.*','marabou','marabou.*']),
     version='0.0.1dev1',
+    zip_safe=False,
     entry_points={
         'console_scripts': ['marabou-train=marabou.scripts.train_chain:main',
                             'marabou-eval=marabou.scripts.seval_chain:main',
                             'marabou-rest-api=marabou.scripts.serve_rest:main']
     },
     install_requires=INSTALL_REQUIREMENTS,
-    #data_files=[('web', ['marabou/scripts/templates/index.html','marabou/scripts/static/css/app.css'])],
     package_data={},
     include_package_data=True)
