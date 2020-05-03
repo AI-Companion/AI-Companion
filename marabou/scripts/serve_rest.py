b168f32e4adc12190d21b0285046aa99f0013c2e
Youssef Azzouz <youssef.azzouz1512@gmail.com>
2020-05-03 05:28:36 +0100

    host and port of app

--- marabou/scripts/serve_rest.py
+++ marabou/scripts/serve_rest.py
@@ -54,7 +54,7 @@ def main():
         args = parse_arguments()
         model = DumbModel.deserialize(args.model_file)  # load model at the beginning once only
         api.add_resource(PredictSentiment, '/',resource_class_args={model})
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0', port=5000)
 
 if __name__ == '__main__':
     main()
