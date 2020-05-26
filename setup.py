d4e537c31834aca705313f35a5b017f8da41aa05
Youssef Azzouz <youssef.azzouz1512@gmail.com>
2020-05-26 04:42:39 +0100

    ui for marabou and sentiment analysis

--- setup.py
+++ setup.py
@@ -37,8 +37,8 @@ setup(name='marabou',
     version='0.0.1dev1',
     zip_safe=False,
     entry_points={
-        'console_scripts': ['marabou-train=marabou.scripts.train_chain:main',
-                            'marabou-eval=marabou.scripts.seval_chain:main',
+        'console_scripts': ['marabou-train=marabou.scripts.train_sentiment_analysis:main',
+                            'marabou-eval=marabou.scripts.eval_sentiment_analysis:main',
                             'marabou-rest-api=marabou.scripts.serve_rest:main']
     },
     install_requires=INSTALL_REQUIREMENTS,
