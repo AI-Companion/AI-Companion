daeb9f4b9e66430a7cfd2aac6c6c836ec2cfb264
Youssef Azzouz <youssef.azzouz1512@gmail.com>
2020-07-26 22:57:17 +0100

    renaming of the app

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -13,7 +13,7 @@ export default class Navigation extends React.Component {
     return (
         <nav className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
         <div className="container">
-            <a className="navbar-brand js-scroll-trigger" href="#page-top">Companion.ai</a><button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+            <a className="navbar-brand js-scroll-trigger" href="#page-top">Ai-Companion</a><button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
             <div className="collapse navbar-collapse" id="navbarResponsive">
                 <ul className="navbar-nav ml-auto my-2 my-lg-0">
                     <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#portfolio">Our Services</a></li>
