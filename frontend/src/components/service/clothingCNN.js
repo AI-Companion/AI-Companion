2b53d6c5fa5e458410fa78a14734c7bb0498bb38
Youssef Azzouz <youssef.azzouz1512@gmail.com>
2020-08-06 00:58:11 +0100

    fixes 2

--- frontend/src/components/service/clothingCNN.js
+++ frontend/src/components/service/clothingCNN.js
@@ -46,29 +46,40 @@ export default class ClothingCNN extends React.Component {
   render() {
     if(this.state.response){
     var out = (<div className="col-lg-5 col-sm-7">
-            <h2>{this.state.response}</h2>
+            <h1>{this.state.response}</h1>
           </div>);
     };
     return (
-      <div className="App">
-        <br/>
-        <form onSubmit={this.handleSubmit}>
-        <div className="row no-gutters">
-          <div className="col-lg-4 col-sm-6">
-            <p>
-              <input type="file"
-                    id="image"
-                    accept="image/png, image/jpeg"  onChange={this.handleImageChange} required/>
-            </p>
-            <input type="submit"/>
-          </div>
-        </div>
-        </form>
-        <div className="row no-gutters">
-          <div className="col-lg-6 col-sm-10">
-            <img alt="" id="output" />	
-          </div>
-          {out}
+      <div>
+        <section className="container-form" id="form">
+            <div className="container">
+                <h2 className="text-center mt-0">Upload an Image to know what it is </h2>
+                <hr className="divider my-4" />
+                <div className="row justify-content-center">
+                    <div className="col-lg-8 text-center">
+                      <form onSubmit={this.handleSubmit}>
+                        <div className="row no-gutters">
+                          <div className="col-lg-8 text-center">
+                            <p>
+                                <input type="file"
+                                    id="image"
+                                    accept="image/png, image/jpeg"  onChange={this.handleImageChange} required/>
+                            </p>
+                            <input type="submit"/>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <br/><br/>
+         <div className="row">
+           <div className="col-lg-6 col-sm-10">
+             <img alt="" id="output" />	
+           </div>
+           {out}
+            
         </div>
       </div>
     );
