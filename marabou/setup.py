4c8984378fc4f346427c18b2efd577b63b0a3079
mmarouen <azzouz.marouen@gmail.com>
2020-11-02 14:27:25 +0100

    refactoring

---
+++ marabou/setup.py
@@ -0,0 +1,54 @@
+import subprocess
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+
+EVALUATION_REQUIREMENTS = [
+    'ds-gear',
+    'flask==1.1.2',
+    'flask_restful',
+    'pillow'
+]
+
+TRAINING_REQUIREMENTS = [
+    'numpy==1.19.0rc2',
+    'pandas==1.0.4',
+    'scikit-learn==0.23.1',
+    'matplotlib',
+    'nltk==3.5',
+    'keras==2.3.1',
+    'tensorflow==2.2.0',
+    'opencv-python'
+]
+
+class InstallCommand(install):
+    """
+    will call activate githooks for install mode
+    """
+    def run(self):
+        subprocess.call("git config core.hooksPath .githooks/", shell=True)
+        install.run(self)
+
+
+setup(name='marabou',
+      packages=find_packages(include=['commons', 'marabou.*']),
+      author='Marouen Azzouz, Youssef Azzouz',
+      author_email='azzouz.marouen@gmail.com, youssef.azzouz1512@gmail.com',
+      version='0.1.0',
+      zip_safe=False,
+      extras_require={
+          'train': TRAINING_REQUIREMENTS,
+      },
+      entry_points={
+          'console_scripts': ['marabou-train-sentiment-analysis=marabou.training.scripts.train_sentiment_analysis:main[train]',
+                              'marabou-train-ner=marabou.training.scripts.train_named_entity_recognition:main[train]',
+                              'marabou-train-fashion-classifier=marabou.training.scripts.train_fashion_classifier:main[train]',
+                              'marabou-eval-server=marabou.evaluation.app:main'
+                              ]
+      },
+      dependency_links=['git+https://www.github.com/keras-team/keras-contrib.git#egg=keras-contrib'],
+      install_requires=EVALUATION_REQUIREMENTS,
+      include_package_data=True,
+      cmdClass={
+          'install': InstallCommand
+      })
